23bcc75b3f7fcbd5f5bda2825de57cafda2d6619
baynop <bayuindrawan95@gmail.com>
2021-03-09 00:44:49 +0700

    tic tac toe scalable user optional board

--- main.js
+++ main.js
@@ -1,419 +1,303 @@
+var turn = 'X';
+var game_type = 3;
+var total_turns = 0;
+var robot = true;
+var finished = false;
 
-var positions = [
-	[0, 0, 0],
-	[0, 0, 0],
-	[0, 0, 0]
-];
+var selections = new Array(); 
+	selections['X'] = new Array();
+	selections['O'] = new Array();
 
-var gameFinished = false;
+var scores = new Array(); 
+	scores['X'] = 0;
+	scores['O'] = 0;
 
-const nPlayer1 = 1;
-const nPlayer2 = 2;
+function resetParams() {
+	turn = 'X';
+	game_type = 3;
+	total_turns = 0;
+	robot = true;
+	finished = false;
 
-var scorePlayer1 = 0;
-var scorePlayer2 = 0;
-
-var startingPlayer = nPlayer1;
-
-const imgPlayer1 = 'url("p1.png")';
-const imgPlayer2 = 'url("p2.png")';
-
-var board = document.getElementById("board");
-var scoreboard = document.getElementById("scoreboard");
-var winnerMessageBox = document.getElementById("winnerMessageBox");
-var playAgainButton  = document.getElementById("playAgainButton");
-var selectDifficulty = document.getElementById("difficulty-select");
-
-var difficulty = selectDifficulty.options[selectDifficulty.selectedIndex].value;
-
-const rows = board.rows.length;
-const cols = board.rows[0].cells.length;
-
-for (var row = 0; row < rows; row++) {
-	for (var col = 0; col < cols; col++) {
-		board.rows[row].cells[col].onclick = function () {
-			onCellClicked(this);
-		};
-	}
+	selections['X'] = new Array();
+	selections['O'] = new Array();
 }
 
-function onCellClicked(tableCell) {
-	if (gameFinished) {
-		return;
-	}
-	if(selectDifficulty.disabled == false ) {
-		difficulty = selectDifficulty.options[selectDifficulty.selectedIndex].value;
-		selectDifficulty.disabled = true;
-	}
-	var content = tableCell.innerHTML.split(",");
-	var row = content[0]
-	var col = content[1]
-	if (positions[row][col] == 0) {
-		tableCell.style.backgroundImage = imgPlayer1;
-		positions[row][col] = nPlayer1
-		var winnerNumber = getTheWinner();
-		if( winnerNumber != -1 ) {
-			showWinner(winnerNumber);
-		} else {
-			robotTurn();
+
+function changeTurn(){
+	if (turn == 'X') turn = 'O';
+	else turn = 'X';
+}
+
+
+function winnerPatterns() {
+	var wins = Array();
+
+
+	if (game_type==3) wins = [ 
+								[11,12,13], [21,22,23], [31,32,33],
+						 		[11,21,31], [12,22,32], [13,23,33], 
+						 		[11,22,33], [13,22,31]
+						 	];
+
+
+
+	if (game_type==4) wins = [ 
+								[12,13,14], [11,12,13], [22,23,24], [21,22,23], [32,33,34], [31,32,33], [42,43,44], [41,42,43],
+						 		[21,31,41], [11,21,31], [22,32,42], [12,22,32], [23,33,43], [13,23,33], [24,34,44], [14,24,34],
+						 		[23,32,41], [14,23,32], [22,33,44], [11,22,33]
+						 	];
+
+
+
+	if (game_type==5) wins = [ 
+								[13,14,15], [12,13,14], [11,12,13], 
+								[23,24,25], [22,23,24], [21,22,23], 
+								[33,34,35], [32,33,34], [31,32,33], 
+								[43,44,45], [42,43,44], [41,42,43], 
+								[53,54,55], [52,53,54], [51,52,53],
+						 		[31,41,51], [21,31,41], [11,21,31], 
+								[32,42,52], [22,32,42], [12,22,32], 
+								[33,43,53], [23,33,43], [13,23,33], 
+								[34,44,54], [24,34,44], [14,24,34], 
+								[35,45,55], [25,35,45], [15,25,35],
+						 		[33,44,55], [22,33,44], [11,22,33], 
+								[33,42,51], [24,33,42], [15,24,33]
+						 	];
+
+	return wins
+}
+
+
+function DefaultRobotPatterns() {
+	var robot_turns = Array();
+
+
+	if (game_type==3) robot_turns = [22,11,33,13,21,23,12,32,31];
+
+
+
+	if (game_type==4) robot_turns = [11,22,33,44,14,13,12,21,31,41,42,43,24,34,32,23];
+
+
+
+	if (game_type==5) robot_turns = [11,22,33,44,55,15,14,13,12,51,41,31,21,35,45,25,53,52,54,42,43,32,34,23,24];
+
+	return robot_turns
+}
+
+
+function checkWinner() {
+
+	var selected = selections[turn].sort();
+	var win_patterns = winnerPatterns();
+
+	finished = false;
+	for (var x=0; x < win_patterns.length; x++) {
+		
+		if (finished != true) { 
+			finished = isWinner(win_patterns[x], selections[turn]);
+
+			if ( finished === true ) {
+				
+			
+				scoreUpdate(turn);
+
+			
+				disableAllBoxes();
+
+				alert('Player '+turn+' Won !!');
+				
+				break;
+			} 
 		}
 	}
-}
 
-function randomInt(min, max) {
-	return Math.floor((Math.random() * max) + min);
-}
 
-function robotTurn() {
-	if (difficulty == "easy") {
-		robotEasy();
-	} else if (difficulty == "hard") {
-		robotHard();
-	} else {
-		robotNormal();
+	if ( ( total_turns == (game_type*game_type) ) && finished === false ) { 
+		alert('Game Draw!');
+		finished = true;
+		disableAllBoxes(); 
 	}
 }
 
-function makeRobotPlay(row, col) {
-	if (row >= 0 && row < rows) {
-		if (col >= 0 && col < cols) {
-		
-			board.rows[row].cells[col].style.backgroundImage = imgPlayer2;
-		
-			positions[row][col] = nPlayer2;
-		
-			var winnerNumber = getTheWinner();
-			if( winnerNumber != -1 ) {
-				showWinner(winnerNumber);
+
+function isWinner(win_pattern, selections){
+
+	var match = 0;
+
+	for (var x=0; x<win_pattern.length; x++) {
+		for (var y=0; y<selections.length; y++) {
+			if (win_pattern[x]==selections[y]) {
+				match++;
 			}
 		}
 	}
+
+	if (match==win_pattern.length) return true;
+
+	return false;
 }
 
-function robotEasy() {
-	if (gameFinished) {
-		return;
-	}
-	var row = -1;
-	var col = -1;
-	var found = false;
 
-	for (var index = 0; index < 10; index++) {
-		var r = randomInt(0,2);
-		var c = randomInt(0,2);
-		if (positions[r][c] == 0) {
-			row = r;
-			col = c;
-			found = true;
+function disableAllBoxes() {
+
+	var elements = document.getElementsByClassName("grid-box");
+	for (var i = 0; i < elements.length; i++) {
+	  elements[i].disabled =true;
+	}
+
+}
+
+
+function resetAIButton() {
+	var checkbox = document.getElementById('robot'); 	
+	checkbox.checked = 'checked';
+}
+
+
+function generateGame(){
+
+
+	resetParams();
+
+
+	game_type = Number(document.getElementById('game_type').value);
+
+
+	robot_object = document.getElementById('robot'); 
+	if (robot_object.checked === true) robot = true; 
+	else  robot = false;
+
+
+	document.getElementById('game-board').innerHTML = '';
+
+
+	for (var row = 1; row <= game_type; row++){
+		for (var col = 1; col <= game_type; col++) {
+			var unique_name = 'grid-'+row+'-'+col;
+			var unique_id = row+''+col;
+			var button = document.createElement("input");
+
+			button.setAttribute("value", ' ');
+			button.setAttribute("id", unique_id);
+			button.setAttribute("name", unique_name);
+			button.setAttribute("class", 'grid-box');
+			button.setAttribute("type", 'button');
+			button.setAttribute("onclick", "markCheck(this)");
+			document.getElementById('game-board').appendChild(button);
+		}
+
+		var breakline = document.createElement("br");
+			document.getElementById('game-board').appendChild(breakline);
+	}
+
+}
+
+
+function markCheck(obj){
+
+	obj.value = turn;
+	total_turns++;
+
+	if (turn == 'X' ) {
+		obj.setAttribute("class", 'green-player');
+	} else {
+		obj.setAttribute("class", 'red-player');
+	}
+
+	obj.setAttribute("disabled", 'disabled');
+	selections[turn].push(Number(obj.id));
+
+	checkWinner();
+	changeTurn();
+
+	if (robot===true) autoTurn();
+}
+
+
+function autoTurn(again=false) {
+
+	is_empty_result = true;
+
+	if (turn === 'X' || finished === true) return false;
+
+	var robot_pattern = '';
+	if (again==true) robot_pattern = DefaultRobotPatterns();
+	else robot_pattern = getAutoTurnPattern(); 
+
+	for(var x = 0; x < robot_pattern.length; x++) {
+		var desired_obj = document.getElementById(robot_pattern[x]);
+		if (desired_obj.value == '' || desired_obj.value == ' ') { 
+			markCheck(desired_obj); 
+			is_empty_result = false;
 			break;
+		} 
+	}
+
+}
+
+function getAutoTurnPattern() {
+
+	var pattern = [];
+	pattern = getMostNearestPattern('O');
+	if (pattern.length <= 0) {
+		pattern = getMostNearestPattern('X');
+		if (pattern.length <= 0) {
+			pattern = DefaultRobotPatterns();
 		}
 	}
 
-	if (!found) {
-		for (var r = 0; r < positions.length; r++) {
-			for (var c = 0; c < positions[r].length; c++) {
-				if (positions[r][c] == 0) {
-					row = r;
-					col = c;
-					found = true;
-					break;
+	return pattern;
+	
+}
+
+
+function getMostNearestPattern(turn){
+
+	var matches = 0;
+
+	var selected = selections[turn].sort();
+	var win_patterns = winnerPatterns();
+
+	finished = false;
+	for (var x=0; x < win_patterns.length; x++) {
+		var intersected = intersectionArray(selected, win_patterns[x]);
+
+		if ( intersected.length==(win_patterns[x].length-1) ) { //return win_patterns[x];
+
+		
+			for (var y=0; y < win_patterns[x].length; y++) {
+				obj = document.getElementById(win_patterns[x][y]);
+				if (obj.value == '' || obj.value == ' ') {
+				
+					return win_patterns[x];	
 				}
 			}
-			if (found == true) {
-				break;
-			}
 		}
-	}
 
-	if (found) {
-		makeRobotPlay(row, col);
 	}
-}
-
-function robotNormal() {
-	if (gameFinished) {
-		return;
-	}
-	var row = -1;
-	var col = -1;
-	var found = false;
-
-	for (var r = 0; r < positions.length; r++) {
-		if (positions[r][0] != 0 &&
-			positions[r][2] == 0 &&
-			positions[r][0] == positions[r][1]) {
-			row = r;
-			col = 2;
-			found = true;
-			break;
-		}
-		if (positions[r][1] != 0 &&
-			positions[r][0] == 0 &&
-			positions[r][1] == positions[r][2]) {
-			row = r;
-			col = 0;
-			found = true;
-			break;
-		}
-		if (positions[r][0] != 0 &&
-			positions[r][1] == 0 &&
-			positions[r][0] == positions[r][2]) {
-			row = r;
-			col = 1;
-			found = true;
-			break;
-		}
-	}
-
-	if (!found) {
-		for (var c = 0; c < positions[0].length; c++) {
-			if (positions[0][c] != 0 &&
-				positions[2][c] == 0 &&
-				positions[0][c] == positions[1][c]) {
-				row = 2;
-				col = c;
-				found = true;
-				break;
-			}
-			if (positions[1][c] != 0 && 
-				positions[0][c] == 0 &&
-				positions[1][c] == positions[2][c]) {
-				row = 0;
-				col = c;
-				found = true;
-				break;
-			}
-			if (positions[0][c] != 0 &&
-				positions[1][c] == 0 &&
-				positions[0][c] == positions[2][c]) {
-				row = 1;
-				col = c;
-				found = true;
-				break;
-			}
-		}
-	}
-	if (!found) {
-		if (positions[0][0] != 0 &&
-			positions[2][2] == 0 &&
-			positions[0][0] == positions[1][1]) {
-			row = 2;
-			col = 2;
-			found = true;
-		} else if (positions[0][0] != 0 &&
-				   positions[1][1] == 0 &&
-				   positions[0][0] == positions[2][2]) {
-			row = 1;
-			col = 1;
-			found = true;
-		} else if (positions[1][1] != 0 &&
-				   positions[0][0] == 0 &&
-				   positions[1][1] == positions[2][2]) {
-			row = 0;
-			col = 0;
-			found = true;
-		} else if (positions[0][2] != 0 &&
-				   positions[2][0] == 0 &&
-				   positions[0][2] == positions[1][1]) {
-			row = 2;
-			col = 0;
-			found = true;
-		} else if (positions[0][2] != 0 &&
-				   positions[1][1] == 0 &&
-				   positions[0][2] == positions[2][0]) {
-			row = 1;
-			col = 1;
-			found = true;
-		} else if (positions[1][1] != 0 &&
-				   positions[0][2] == 0 &&
-				   positions[1][1] == positions[2][0]) {
-			row = 0;
-			col = 2;
-			found = true;
-		}
-	}
-	if (!found) {
-		robotEasy();
-	} else if (found) {
-		makeRobotPlay(row, col);
-	}
-}
-
-function robotHard() {
-	robotNormal();
+	return [];
 }
 
 
-function isRowFilled(row) {
-	if (positions[row][0] != 0 && 
-		positions[row][0] == positions[row][1] && 
-		positions[row][1] == positions[row][2]) {
-		return true;
-	}
-	return false;
+function intersectionArray(x, o){
+
+    var response = [];
+    for (var i = 0; i < x.length; i++) {
+        for (var z = 0; z < o.length; z++) {
+            if (x[i] == o[z]) {
+                response.push(x[i]);
+                break;
+            }
+        }
+    }
+    return response;
+
 }
 
-function isColumnFilled(col) {
-	if (positions[0][col] != 0 &&
-		positions[0][col] == positions[1][col] &&
-		positions[1][col] == positions[2][col]) {
-		return true;
-	}
-	return false;
-}
 
-function isDiagonalFilled(diag) {
-	if (diag == 1) {
-		if (positions[0][0] != 0 &&
-			positions[0][0] == positions[1][1] &&
-			positions[1][1] == positions[2][2]) {
-			return true;
-		}
-	} else if (diag == 2) {
-		if (positions[2][0] != 0 &&
-			positions[2][0] == positions[1][1] &&
-			positions[1][1] == positions[0][2]) {
-			return true;
-		}
-	}
-	return false;
-}
-
-function getTheWinner() {
-	if (isRowFilled(0)) {
-		applyOpacityRow(0);
-		return positions[0][0];
-	} 
-	if (isRowFilled(1)) {
-		applyOpacityRow(1);
-		return positions[1][0];
-	} 
-	if (isRowFilled(2)) {
-		applyOpacityRow(2);
-		return positions[2][0];
-	} 
-	if (isColumnFilled(0)) {
-		applyOpacityColumn(0);
-		return positions[0][0];
-	} 
-	if (isColumnFilled(1)) {
-		applyOpacityColumn(1);
-		return positions[0][1];
-	}
-	if (isColumnFilled(2)) {
-		applyOpacityColumn(2);
-		return positions[0][2];
-	} 
-	if (isDiagonalFilled(1)) {
-		applyOpacityDiagonal(1);
-		return positions[0][0];
-	} 
-	if (isDiagonalFilled(2)) {
-		applyOpacityDiagonal(2);
-		return positions[2][0];
-	} 
-
-	for (var row = 0; row < positions.length; row++) {
-		for (var col = 0; col < positions[row].length; col++) {
-			if (positions[row][col] == 0) {
-				return -1;
-			}
-		}
-	}
-	
-	return 0;
-}
-
-function applyOpacity() {
-	for (var row = 0; row < rows; row++) {
-		for (var col = 0; col < cols; col++) {
-			board.rows[row].cells[col].style.opacity = 0.5;
-		}
-	}
-}
-
-function applyOpacityRow(row) {
-	applyOpacity();
-	for (var col = 0; col < cols; col++) {
-		board.rows[row].cells[col].style.opacity = 1.0;
-	}
-}
-
-function applyOpacityColumn(col) {
-	applyOpacity();
-	for (var row = 0; row < rows; row++) {
-		board.rows[row].cells[col].style.opacity = 1.0;
-	}
-}
-
-function applyOpacityDiagonal(diag) {
-	applyOpacity();
-	if (diag == 1) {
-		board.rows[0].cells[0].style.opacity = 1.0;
-		board.rows[1].cells[1].style.opacity = 1.0;
-		board.rows[2].cells[2].style.opacity = 1.0;
-	} else {
-		board.rows[0].cells[2].style.opacity = 1.0;
-		board.rows[1].cells[1].style.opacity = 1.0;
-		board.rows[2].cells[0].style.opacity = 1.0;
-	}
-}
-
-function showWinner(winnerNumber) {
-
-	gameFinished = true;
-
-	if (winnerNumber == nPlayer1) {
-		scorePlayer1 += 1;
-		scoreboard.rows[1].cells[0].innerHTML = scorePlayer1;
-		winnerMessageBox.innerHTML = "YOU WIN!";
-	} else if (winnerNumber == nPlayer2) {
-		scorePlayer2 += 1;
-		scoreboard.rows[1].cells[1].innerHTML = scorePlayer2;
-		winnerMessageBox.innerHTML = "YOU LOSE!";
-	} else if (winnerNumber == 0) {
-		winnerMessageBox.innerHTML = "DRAW!";
-	}
-
-	winnerMessageBox.style.display = 'block';
-	playAgainButton.style.display  = 'block';
-	selectDifficulty.disabled = false;
-}
-
-function reset() {
-
-	positions = [
-		[0, 0, 0],
-		[0, 0, 0],
-		[0, 0, 0]
-	];
-
-
-	for (var row = 0; row < rows; row++) {
-		for (var col = 0; col < cols; col++) {
-			board.rows[row].cells[col].style.backgroundImage = '';
-			board.rows[row].cells[col].style.opacity = 1.0;
-		}
-	}
-
-
-	winnerMessageBox.innerHTML = "";
-
-	winnerMessageBox.style.display = 'none';
-	playAgainButton.style.display  = 'none';
-
-
-	gameFinished = false;
-
-
-	difficulty = selectDifficulty.options[selectDifficulty.selectedIndex].value;
-	selectDifficulty.disabled = true;
-
-
-	if (startingPlayer == nPlayer1) {
-		startingPlayer = nPlayer2;
-		robotTurn();
-	} else {
-		startingPlayer = nPlayer1;
-	}
+function scoreUpdate(turn){
+	scores[turn]++;
+	document.getElementById('score-'+turn).innerHTML = scores[turn];
 }
